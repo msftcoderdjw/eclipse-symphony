9c265662fb4b0b3cdfc044fe92f95ca0ac4083ad
Jiawei Du <jiaweidu_js@163.com>
2024-07-08 20:55:35 +0800

    fix caller in logrus

--- coa/pkg/logger/coa-logger.go
+++ coa/pkg/logger/coa-logger.go
@@ -8,7 +8,9 @@ package logger
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"runtime"
 	"sync"
 	"time"
 
@@ -27,6 +29,8 @@ type daprLogger struct {
 	sharedFieldsLock sync.Mutex
 	// sharedFields is the fields that are shared among loggers
 	sharedFields logrus.Fields
+
+	callerSkip uint
 }
 
 var DaprVersion string = "unknown"
@@ -43,6 +47,7 @@ func newDaprLogger(name string) *daprLogger {
 			logFieldScope: name,
 			logFieldType:  LogTypeLog,
 		},
+		callerSkip: 2, // skip 2 frames to get the caller of log functions
 	}
 
 	dl.EnableJSONOutput(defaultJSONOutput)
@@ -85,7 +90,7 @@ func (l *daprLogger) EnableJSONOutput(enabled bool) {
 	}
 
 	l.logger.SetFormatter(formatter)
-	l.logger.SetReportCaller(true)
+	// l.logger.SetReportCaller(true)
 }
 
 // SetAppID sets app_id field in the log. Default value is empty string
@@ -122,100 +127,107 @@ func (l *daprLogger) GetSharedFields() logrus.Fields {
 
 // Info logs a message at level Info.
 func (l *daprLogger) InfoCtx(ctx context.Context, args ...interface{}) {
-	l.logger.WithContext(ctx).WithFields(l.GetSharedFields()).Log(logrus.InfoLevel, args...)
+	l.logger.WithContext(ctx).WithFields(l.GetSharedFields()).WithField("caller", getCaller(int(l.callerSkip))).Log(logrus.InfoLevel, args...)
 }
 
 // Infof logs a message at level Info.
 func (l *daprLogger) InfofCtx(ctx context.Context, format string, args ...interface{}) {
-	l.logger.WithContext(ctx).WithFields(l.GetSharedFields()).Logf(logrus.InfoLevel, format, args...)
+	l.logger.WithContext(ctx).WithFields(l.GetSharedFields()).WithField("caller", getCaller(int(l.callerSkip))).Logf(logrus.InfoLevel, format, args...)
 }
 
 // Debug logs a message at level Debug.
 func (l *daprLogger) DebugCtx(ctx context.Context, args ...interface{}) {
-	l.logger.WithContext(ctx).WithFields(l.GetSharedFields()).Log(logrus.DebugLevel, args...)
+	l.logger.WithContext(ctx).WithFields(l.GetSharedFields()).WithField("caller", getCaller(int(l.callerSkip))).Log(logrus.DebugLevel, args...)
 }
 
 // Debugf logs a message at level Debug.
 func (l *daprLogger) DebugfCtx(ctx context.Context, format string, args ...interface{}) {
-	l.logger.WithContext(ctx).WithFields(l.GetSharedFields()).Logf(logrus.DebugLevel, format, args...)
+	l.logger.WithContext(ctx).WithFields(l.GetSharedFields()).WithField("caller", getCaller(int(l.callerSkip))).Logf(logrus.DebugLevel, format, args...)
 }
 
 // Warn logs a message at level Warn.
 func (l *daprLogger) WarnCtx(ctx context.Context, args ...interface{}) {
-	l.logger.WithContext(ctx).WithFields(l.GetSharedFields()).Log(logrus.WarnLevel, args...)
+	l.logger.WithContext(ctx).WithFields(l.GetSharedFields()).WithField("caller", getCaller(int(l.callerSkip))).Log(logrus.WarnLevel, args...)
 }
 
 // Warnf logs a message at level Warn.
 func (l *daprLogger) WarnfCtx(ctx context.Context, format string, args ...interface{}) {
-	l.logger.WithContext(ctx).WithFields(l.GetSharedFields()).Logf(logrus.WarnLevel, format, args...)
+	l.logger.WithContext(ctx).WithFields(l.GetSharedFields()).WithField("caller", getCaller(int(l.callerSkip))).Logf(logrus.WarnLevel, format, args...)
 }
 
 // Error logs a message at level Error.
 func (l *daprLogger) ErrorCtx(ctx context.Context, args ...interface{}) {
-	l.logger.WithContext(ctx).WithFields(l.GetSharedFields()).Log(logrus.ErrorLevel, args...)
+	l.logger.WithContext(ctx).WithFields(l.GetSharedFields()).WithField("caller", getCaller(int(l.callerSkip))).Log(logrus.ErrorLevel, args...)
 }
 
 // Errorf logs a message at level Error.
 func (l *daprLogger) ErrorfCtx(ctx context.Context, format string, args ...interface{}) {
-	l.logger.WithContext(ctx).WithFields(l.GetSharedFields()).Logf(logrus.ErrorLevel, format, args...)
+	l.logger.WithContext(ctx).WithFields(l.GetSharedFields()).WithField("caller", getCaller(int(l.callerSkip))).Logf(logrus.ErrorLevel, format, args...)
 }
 
 // Fatal logs a message at level Fatal then the process will exit with status set to 1.
 func (l *daprLogger) FatalCtx(ctx context.Context, args ...interface{}) {
-	l.logger.WithContext(ctx).WithFields(l.GetSharedFields()).Fatal(args...)
+	l.logger.WithContext(ctx).WithFields(l.GetSharedFields()).WithField("caller", getCaller(int(l.callerSkip))).Fatal(args...)
 }
 
 // Fatalf logs a message at level Fatal then the process will exit with status set to 1.
 func (l *daprLogger) FatalfCtx(ctx context.Context, format string, args ...interface{}) {
-	l.logger.WithContext(ctx).WithFields(l.GetSharedFields()).Fatalf(format, args...)
+	l.logger.WithContext(ctx).WithFields(l.GetSharedFields()).WithField("caller", getCaller(int(l.callerSkip))).Fatalf(format, args...)
 }
 
 // Info logs a message at level Info.
 func (l *daprLogger) Info(args ...interface{}) {
-	l.logger.WithFields(l.GetSharedFields()).Log(logrus.InfoLevel, args...)
+	l.logger.WithFields(l.GetSharedFields()).WithField("caller", getCaller(int(l.callerSkip))).Log(logrus.InfoLevel, args...)
 }
 
 // Infof logs a message at level Info.
 func (l *daprLogger) Infof(format string, args ...interface{}) {
-	l.logger.WithFields(l.GetSharedFields()).Logf(logrus.InfoLevel, format, args...)
+	l.logger.WithFields(l.GetSharedFields()).WithField("caller", getCaller(int(l.callerSkip))).Logf(logrus.InfoLevel, format, args...)
 }
 
 // Debug logs a message at level Debug.
 func (l *daprLogger) Debug(args ...interface{}) {
-	l.logger.WithFields(l.GetSharedFields()).Log(logrus.DebugLevel, args...)
+	l.logger.WithFields(l.GetSharedFields()).WithField("caller", getCaller(int(l.callerSkip))).Log(logrus.DebugLevel, args...)
 }
 
 // Debugf logs a message at level Debug.
 func (l *daprLogger) Debugf(format string, args ...interface{}) {
-	l.logger.WithFields(l.GetSharedFields()).Logf(logrus.DebugLevel, format, args...)
+	l.logger.WithFields(l.GetSharedFields()).WithField("caller", getCaller(int(l.callerSkip))).Logf(logrus.DebugLevel, format, args...)
 }
 
 // Warn logs a message at level Warn.
 func (l *daprLogger) Warn(args ...interface{}) {
-	l.logger.WithFields(l.GetSharedFields()).Log(logrus.WarnLevel, args...)
+	l.logger.WithFields(l.GetSharedFields()).WithField("caller", getCaller(int(l.callerSkip))).Log(logrus.WarnLevel, args...)
 }
 
 // Warnf logs a message at level Warn.
 func (l *daprLogger) Warnf(format string, args ...interface{}) {
-	l.logger.WithFields(l.GetSharedFields()).Logf(logrus.WarnLevel, format, args...)
+	l.logger.WithFields(l.GetSharedFields()).WithField("caller", getCaller(int(l.callerSkip))).Logf(logrus.WarnLevel, format, args...)
 }
 
 // Error logs a message at level Error.
 func (l *daprLogger) Error(args ...interface{}) {
-	l.logger.WithFields(l.GetSharedFields()).Log(logrus.ErrorLevel, args...)
+	l.logger.WithFields(l.GetSharedFields()).WithField("caller", getCaller(int(l.callerSkip))).Log(logrus.ErrorLevel, args...)
 }
 
 // Errorf logs a message at level Error.
 func (l *daprLogger) Errorf(format string, args ...interface{}) {
-	l.logger.WithFields(l.GetSharedFields()).Logf(logrus.ErrorLevel, format, args...)
+	l.logger.WithFields(l.GetSharedFields()).WithField("caller", getCaller(int(l.callerSkip))).Logf(logrus.ErrorLevel, format, args...)
 }
 
 // Fatal logs a message at level Fatal then the process will exit with status set to 1.
 func (l *daprLogger) Fatal(args ...interface{}) {
-	l.logger.WithFields(l.GetSharedFields()).Fatal(args...)
+	l.logger.WithFields(l.GetSharedFields()).WithField("caller", getCaller(int(l.callerSkip))).Fatal(args...)
 }
 
 // Fatalf logs a message at level Fatal then the process will exit with status set to 1.
 func (l *daprLogger) Fatalf(format string, args ...interface{}) {
-	l.logger.WithFields(l.GetSharedFields()).Fatalf(format, args...)
+	l.logger.WithFields(l.GetSharedFields()).WithField("caller", getCaller(int(l.callerSkip))).Fatalf(format, args...)
+}
+
+func getCaller(extraSkip int) string {
+	callerPc := make([]uintptr, 1)
+	runtime.Callers(1+extraSkip, callerPc) // skipping caller of getCaller().
+	callerFrame, _ := runtime.CallersFrames(callerPc).Next()
+	return fmt.Sprintf("%s:%d", callerFrame.Function, callerFrame.Line)
 }
