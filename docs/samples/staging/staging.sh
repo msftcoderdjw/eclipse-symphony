7515a8218a0b2d056cd98ec44ec453ad42b320ce
Jiawei Du <jiaweidu_js@163.com>
2024-07-30 03:03:45 +0800

    test

--- docs/samples/staging/staging.sh
+++ docs/samples/staging/staging.sh
@@ -9,7 +9,7 @@ inputs_file=$1
 
 output_file="${inputs_file%.*}-output.${inputs_file##*.}"
 
-cat $inputs_file | tee -a abc.json
+cat $inputs_file | jq -c '.' > abc.json
 
 # approve by returning no errors and a JSON object with status
 echo "{\"status\":200}" | jq -c '.' > "$output_file" 
