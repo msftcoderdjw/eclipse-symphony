a6729910ecf1259534a8711a8af606e7c5d327ea
Jiawei Du <jiaweidu_js@163.com>
2024-07-30 13:09:51 +0800

    remove docker dependencies

--- docs/samples/staging/staging.sh
+++ docs/samples/staging/staging.sh
@@ -42,19 +42,19 @@ harbor_user=$(cat $inputs_file | jq -r .harbor_user)
 source_images=$(cat $inputs_file | jq -r .source_images)
 
 # Install docker, nslookup, and skopeo
-echo "Install docker" | tee -a $logFile
-apt-get update
-apt-get install ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# echo "Install docker" | tee -a $logFile
+# apt-get update
+# apt-get install ca-certificates curl
+# install -m 0755 -d /etc/apt/keyrings
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+# chmod a+r /etc/apt/keyrings/docker.asc
+# # Add the repository to Apt sources:
+# echo \
+#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+#   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+#   tee /etc/apt/sources.list.d/docker.list > /dev/null
+# apt-get update
+# apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 echo "Install dnsutils (nslookup)" | tee -a $logFile
 apt-get install -y dnsutils
 echo "Install skopeo" | tee -a $logFile
