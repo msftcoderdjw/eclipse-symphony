846dbdeaf855911bc7f4b46f834d611beb2fc40b
Jiawei Du <jiaweidu_js@163.com>
2024-07-31 19:23:41 +0800

    fix comments

--- api/pkg/apis/v1alpha1/managers/stage/stage-manager.go
+++ api/pkg/apis/v1alpha1/managers/stage/stage-manager.go
@@ -426,7 +426,6 @@ func (s *StageManager) HandleTriggerEvent(ctx context.Context, campaign model.Ca
 
 			eCtx := s.VendorContext.EvaluationContext.Clone()
 			eCtx.Inputs = triggerData.Inputs
-			log.DebugfCtx(ctx, " M (Stage): HandleTriggerEvent evaluation inputs 1: %v", eCtx.Inputs)
 			if eCtx.Inputs != nil {
 				if v, ok := eCtx.Inputs["context"]; ok {
 					eCtx.Value = v
@@ -474,8 +473,6 @@ func (s *StageManager) HandleTriggerEvent(ctx context.Context, campaign model.Ca
 			}
 		}
 
-		log.DebugfCtx(ctx, " M (Stage): HandleTriggerEvent before evaluation inputs 2: %v", inputs)
-
 		// inject default inputs
 		inputs["__campaign"] = triggerData.Campaign
 		inputs["__namespace"] = triggerData.Namespace
@@ -508,8 +505,6 @@ func (s *StageManager) HandleTriggerEvent(ctx context.Context, campaign model.Ca
 				}
 			}
 		}
-
-		log.DebugfCtx(ctx, " M (Stage): HandleTriggerEvent after evaluation inputs 2: %v", inputs)
 
 		factory := symproviders.SymphonyProviderFactory{}
 		var provider providers.IProvider
