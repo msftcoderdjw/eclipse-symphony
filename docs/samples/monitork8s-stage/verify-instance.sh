622b84a2c214154ccbef13bcaa18b618296d007a
Jiawei Du <jiaweidu_js@163.com>
2024-09-09 14:45:16 +0800

    new samples

--- docs/samples/monitork8s-stage/verify-instance.sh
+++ docs/samples/monitork8s-stage/verify-instance.sh
@@ -2,4 +2,19 @@ inputs_file=$1
 
 output_file="${inputs_file%.*}-output.${inputs_file##*.}"
 
-echo "{\"status\":200}" | jq -c '.' > "$output_file"
+items=$(jq -r '.items[]' < "$inputs_file")
+instance_name=$(jq -r '.instanceName' < "$inputs_file")
+
+# get the instance object
+for item in $items; do
+    status=$item.status.provisioningStatus.status
+    if [ "$status" == "Succeeded" ]; then
+        echo "{\"status\":200}" | jq -c '.' > "$output_file"
+        exit 0
+    elif [ "$status" == "Failed" ]; then
+        echo "{\"status\":500}" | jq -c '.' > "$output_file"
+        exit 0
+    fi
+done
+
+echo "{\"status\":202}" | jq -c '.' > "$output_file"
