e61639bd455673594ac56eabfdb76b551c1a1de4
Jiawei Du <jiadu@microsoft.com>
2024-09-24 20:26:55 +0800

    add tunnel setup for maestro sample tests

--- test/integration/scenarios/07.maestroTest/magefile.go
+++ test/integration/scenarios/07.maestroTest/magefile.go
@@ -50,9 +50,9 @@ var (
 func Test() error {
 	fmt.Println("Running ", TEST_NAME)
 
-	defer testhelpers.Cleanup(TEST_NAME)
+	cancel, tunnelPid, err := testhelpers.SetupClusterWithTunnel()
+	defer testhelpers.CleanupWithTunnel(cancel, tunnelPid, TEST_NAME)
 
-	err := testhelpers.SetupCluster()
 	if err != nil {
 		return err
 	}
